db8f305245adb8dcff7349416842fdba4a38697e
AmandaJisoo <amandapark8988@gmail.com>
2022-04-14 01:17:10 -0700

    google img url upload to s3 is completed

--- src/pages/APIStorage.js
+++ src/pages/APIStorage.js
@@ -13,18 +13,20 @@ export default class APIStorage {
         }
     }
 
-    async uploadImageToS3(file) {
+    async uploadImageToS3AndGeneratePresignedURL(file) {
         try {
+            //upload it to the s3
             const res = await Storage.put(file.name, file, {
                 contentType: file.type,
                 level: "private",
             });
-            const credentials = await Auth.currentUserCredentials();
-            const identityId = credentials.identityId;
-            const path_to_image = `private/${identityId}/${res.key}`;
-            console.log("path_to_image", path_to_image);
-            //ex)private/us-east-1:bc3dbc1e-2f58-47a2-824f-28ee91c077d5/Screen Shot 2022-03-10 at 12.13.54 AM.png
-            return path_to_image;
+            const key = `${res.key}`;
+            //get the presigned_url
+            const presignedURL = await Storage.get(key, {
+                level: "private",
+                expires: 3600,
+            });
+            return presignedURL;
         } catch (err) {
             console.log(err);
         }
