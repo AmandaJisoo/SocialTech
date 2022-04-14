cb7696230fd7c3354d2addb4b91ef475b4545fc0
AmandaJisoo <amandapark8988@gmail.com>
2022-04-14 01:45:43 -0700

    UpsertPostWithURL for google url link is created'

--- src/pages/APIStorage.js
+++ src/pages/APIStorage.js
@@ -13,30 +13,37 @@ export default class APIStorage {
         }
     }
 
-    async uploadImageToS3AndGeneratePresignedURL(file) {
+    //still for local file upload
+    async uploadImageToS3(file) {
         try {
-            //upload it to the s3
             const res = await Storage.put(file.name, file, {
                 contentType: file.type,
                 level: "private",
             });
-            const key = `${res.key}`;
-            //get the presigned_url
-            const presignedURL = await Storage.get(key, {
-                level: "private",
-                expires: 3600,
-            });
-            return presignedURL;
+            const credentials = await Auth.currentUserCredentials();
+            const identityId = credentials.identityId;
+            const path_to_image = `private/${identityId}/${res.key}`;
+            console.log("path_to_image", path_to_image);
+            //ex)private/us-east-1:bc3dbc1e-2f58-47a2-824f-28ee91c077d5/Screen Shot 2022-03-10 at 12.13.54 AM.png
+            return path_to_image;
         } catch (err) {
             console.log(err);
         }
     }
 
-    //TODO Lynos: you can set up google map and call this end point to save shelters to our db
-    //check backend documentation for object form 
     async upsertPost(postInformation) {
         try {
             return await API.post('SocialTechService', "/UpsertPost", {
+                body: postInformation
+            })
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async upsertPostWithURL(postInformation) {
+        try {
+            return await API.post('SocialTechService', "/UpsertPostWithURL", {
                 body: postInformation
             })
         } catch (err) {
