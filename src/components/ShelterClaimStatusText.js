c763d9f420311754a3c7ccd4a94478e556244d97
AmandaJisoo <amandapark8988@gmail.com>
2022-05-02 17:16:02 -0700

    different UI depending on claim status

--- src/components/ShelterClaimStatusText.js
+++ src/components/ShelterClaimStatusText.js
@@ -1,24 +1,58 @@
 import { React } from 'react';
-import Typography from '@mui/material/Typography';
 import appTheme from '../theme/appTheme.json';
+import InfoIcon from '@mui/icons-material/Info';
+import { Grid, Button, Typography } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import PendingIcon from '@mui/icons-material/Pending';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 const ShelterClaimStatusText = ({ claim_status }) => {
-    let background, color, text
+    let background, color, text, val
     if (claim_status === "no_claim") {
-        text = "Unclaimed Shelter"
+        val = 
+        <Grid container direction="row" alignItems="center">
+            <Grid item>
+            <IconButton aria-label="business claim status">
+                <InfoIcon style={{ color: '#48AAAD' }}/>
+            </IconButton>
+            </Grid>
+            <Grid item>
+                <Typography style={{fontSize: "17px", color: '#48AAAD' }}>
+                Unclaimed
+                </Typography>
+            </Grid>
+        </Grid>
     } else if (claim_status === "pending") {
-        text = "Claim In Progress"
+        val = 
+        <Grid container direction="row" alignItems="center">
+            <Grid item>
+                <IconButton aria-label="business claim status">
+                    <PendingIcon style={{ color: '#48AAAD' }}/>
+            </IconButton>
+            </Grid>
+            <Grid item>
+                <Typography style={{fontSize: "17px", color: '#48AAAD' }}>
+                    Pending Verification
+                </Typography>
+            </Grid>
+        </Grid>
     } else {
-        text = "Claimed Shelter"
-
+        val = 
+        <Grid container direction="row" alignItems="center">
+            <Grid item>
+            <IconButton aria-label="business claim status">
+                <CheckCircleIcon style={{ color: '#48AAAD' }}/>
+            </IconButton>
+        </Grid>
+        <Grid item>
+            <Typography style={{fontSize: "17px", color: '#48AAAD' }}>
+                Claimed 
+            </Typography>
+        </Grid>
+    </Grid>
     }
 
-    return (
-    <Typography style={{fontSize: "17px", color: color}}>
-        Business Status: {text}
-    </Typography>
-    )
-    
+    return val
 };
 
 export default ShelterClaimStatusText;
